1cb655c113747fb969089d960d6d9dc20a02aacd
Meder <medalon07@gmail.com>
2022-11-23 11:58:39 +0600

    added icon btns

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopapp/screens/products_overview_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,25 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, 
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop'),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
