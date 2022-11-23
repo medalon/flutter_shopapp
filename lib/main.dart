367f33aff2891e70eb0457d94dd8a8deead86b51
Meder <medalon07@gmail.com>
2022-11-23 12:31:27 +0600

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shopapp/screens/products_overview_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final ThemeData theme = ThemeData(
+    primarySwatch: Colors.purple,
+    fontFamily: 'Lato',
+  );
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue, 
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(secondary: Colors.deepOrange),
       ),
       home: ProductsOverviewScreen(),
     );
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,28 +9,33 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.favorite,
-          ),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: const Icon(
-            Icons.shopping_cart,
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.favorite,
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
           ),
-          onPressed: () {},
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(
+              Icons.shopping_cart,
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
         ),
       ),
     );
